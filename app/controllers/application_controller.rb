1d8af29777f4dfc4726bce1ad32f390a6130aeb8
dialloflatno <brahimdiallo23@gmail.com>
2021-12-22 03:52:37 -0500

    commit tree 0f8acff11dd7149dbb10ead6b35827bbb6a0ea4e

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,19 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  before_action :plant, only:[:show,:update,:destroy]
 
 end
+
+
+private
+
+    def plant
+      Plant.find(params[:id])
+    end 
+    def add_plant
+      Plant.create(plant_params)
+    end 
+
+    def plant_params
+      params.permit(:name, :image, :price, :is_in_stock)
+    end
--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -3,24 +3,29 @@ class PlantsController < ApplicationController
   # GET /plants
   def index
     plants = Plant.all
-    render json: plants
+    render json: plants, status: :ok
   end
 
   # GET /plants/:id
   def show
-    plant = Plant.find_by(id: params[:id])
-    render json: plant
+    render json: plant, status: :found
   end
 
   # POST /plants
   def create
-    plant = Plant.create(plant_params)
-    render json: plant, status: :created
+    render json: add_plant, status: :created
   end
 
-  private
-
-  def plant_params
-    params.permit(:name, :image, :price, :is_in_stock)
+  def update
+    uplant = plant
+    uplant.update(plant_params)
+    render json: uplant, status: 203
   end
+
+  def destroy
+   unseed = plant
+   unseed.delete
+   render json: unseed, status: :no_content
+  end
+
 end
